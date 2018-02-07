4bda74900fc916b29fcf832d1aa5803d2b710eef
kominx <kominxlpa@gmail.com>
2018-02-07 20:59:04 +0000

    adding remote package from goland

--- higo.go
+++ higo.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/golang/example/stringutil"
+)
 
 func main() {
 	fmt.Printf("Bájate de la higuera.\n")
+	fmt.Println(stringutil.Reverse("Bájate de la higuera."))
 }
