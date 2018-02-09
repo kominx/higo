e0bc344c968f35eabbd1b2364cf080316f7bf26c
kominx <kominxlpa@gmail.com>
2018-02-09 17:36:44 +0000

    From VSCode

--- higo.go
+++ higo.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/golang/example/stringutil"
+	//"github.com/golang/example/stringutil"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("Bájate de la higuera."))
+
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s hostname\n", os.Args[0])
+		fmt.Println("Usage: ", os.Args[0], "hostname")
+		os.Exit(1)
+	}
+	name := os.Args[1]
+
+	addr, err := net.ResolveIPAddr("ip", name)
+	if err != nil {
+		fmt.Println("Resolution error", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Resolved address is ", addr.String())
+	os.Exit(0)
+	//fmt.Println(stringutil.Reverse("Bájate de la higuera."))
 }
